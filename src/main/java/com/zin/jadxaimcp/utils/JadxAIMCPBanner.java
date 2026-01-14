b513b90b22335d9f1679cf713af45dce6b94d8b8
xjoker <wwj0110@hotmail.com>
2026-01-14 19:47:57 +0800

    chore: update BUILD_COMMIT to 2877e7b

--- src/main/java/com/zin/jadxaimcp/utils/JadxAIMCPBanner.java
+++ src/main/java/com/zin/jadxaimcp/utils/JadxAIMCPBanner.java
@@ -15,7 +15,7 @@ public class JadxAIMCPBanner {
      * Build commit SHA - short hash for quick verification
      * Updated by CI/CD pipeline during build
      */
-    public static final String BUILD_COMMIT = "7392f0e";
+    public static final String BUILD_COMMIT = "2877e7b";
     
     /**
      * Original author
